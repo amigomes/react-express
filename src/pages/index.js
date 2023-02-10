0f00426ee58490ecadd8e402b6df985dccbe2015
chuck <charles2588@gmail.com>
2023-02-09 22:55:22 -0800

    commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,11 @@
 import React from 'react';
 import './css/index.css';
-<<<<<<< HEAD
 import Tools from '../components/Tools';
-=======
->>>>>>> 4ac2412d9a84ad4507160f250ad5fddc2629f582
 // const resources = process.env.PUBLIC_URL;
 
 const Home = () => {
 return (
-<<<<<<< HEAD
 		<Tools/>
-=======
-	<div className='main'
-	style={{
-		display: 'flex',
-		justifyContent: 'Right',
-		alignItems: 'Right',
-		height: '100vh',
-	}
-	}>
-		</div>
->>>>>>> 4ac2412d9a84ad4507160f250ad5fddc2629f582
 );
 };
 
