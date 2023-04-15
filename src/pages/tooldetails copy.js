16f9037c31e2c79e9cdd7450c9e65188dbcd39b8
chuck <charles2588@gmail.com>
2023-04-15 14:11:40 -0700

    testing the tooldetails with submit

---
+++ src/pages/tooldetails copy.js
@@ -0,0 +1,79 @@
+import {
+    useParams,
+} from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { CircularProgress,} from "@mui/material";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles({
+    toolHeading : {
+        fontWeight: 900,
+        fontSize: "large",
+    },
+    detailscontainer: {
+        display: "flex",
+        alignItems: "center",
+        flexFlow: "column",
+        marginLeft: "15%",
+        marginRight: "15%",
+    },
+    tooldetailscontainer: {
+        display: "flex",
+        alignItems: "center",
+        flexFlow: "row",
+        marginLeft: "25%",
+        marginRight: "25%",
+        flexGrow:0,
+        flexShrink:0,
+    }
+})
+const ToolDetails = () => {
+    const classes = useStyles();
+    const { toolname } = useParams();
+    const [toolDescription, settoolDescription] = useState();
+    const [imgUrl, setimgUrl] = useState();
+    const [websiteUrl, setwebsiteUrl] = useState();
+
+    const fetchPrimaryPokemonData = async () => {
+        try {
+            if (toolname) {
+                await axios
+                    .get("https://concerned-worm-handbag.cyclic.app/aitools?toolName=" + toolname)
+                    .then((response) => {
+                        const data = response.data[0];
+                        console.log(data);
+                        settoolDescription(data.toolDescription);
+                        console.log(toolDescription);
+                        setimgUrl("https://cdn.sanity.io/images/u0v1th4q/production/" + data.ImageUrl);
+                        setwebsiteUrl(data.websiteUrl);
+                    });
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    };
+    useEffect(() => { fetchPrimaryPokemonData(); }, []);
+
+    return (toolDescription != "" ? (
+        <div className={classes.detailscontainer}>
+            <a href={websiteUrl}><div className="toolHeading">{toolname}</div></a>
+            <div className={classes.tooldetailscontainer}>
+                <div className="imagebox"><img src={imgUrl}/></div>
+                <div>{toolDescription}</div>
+                <div>
+                    {websiteUrl}
+                </div>
+            </div>
+        </div>
+    ) :
+        (
+            <CircularProgress
+                color={"success"}
+                size={200}
+            />
+        )
+    );
+}
+
+export default ToolDetails;
--- src/pages/tooldetails.js
+++ src/pages/tooldetails.js
@@ -1,83 +1,45 @@
-import {
-    useParams,
-} from "react-router-dom";
-import { useEffect, useState } from "react";
-import axios from "axios";
-import { CircularProgress, LinearProgress } from "@mui/material";
-import Box from '@mui/material/Box';
-import {
-    CardMedia,
-  } from "@material-ui/core";
-import { makeStyles } from "@material-ui/styles";
+import React from 'react';
+import { Grid, TextField, Button, Card, CardContent, Typography } from '@material-ui/core';
 
-const useStyles = makeStyles({
-    toolHeading : {
-        fontWeight: 900,
-        fontSize: "large",
-    },
-    detailscontainer: {
-        display: "flex",
-        alignItems: "center",
-        flexFlow: "column",
-        marginLeft: "15%",
-        marginRight: "15%",
-    },
-    tooldetailscontainer: {
-        display: "flex",
-        alignItems: "center",
-        flexFlow: "row",
-        marginLeft: "25%",
-        marginRight: "25%",
-        flexGrow:0,
-        flexShrink:0,
-    }
-})
 const ToolDetails = () => {
-    const classes = useStyles();
-    const { toolname } = useParams();
-    const [toolDescription, settoolDescription] = useState();
-    const [imgUrl, setimgUrl] = useState();
-    const [websiteUrl, setwebsiteUrl] = useState();
-
-    const fetchPrimaryPokemonData = async () => {
-        try {
-            if (toolname) {
-                await axios
-                    .get("https://concerned-worm-handbag.cyclic.app/aitools?toolName=" + toolname)
-                    .then((response) => {
-                        const data = response.data[0];
-                        console.log(data);
-                        settoolDescription(data.toolDescription);
-                        console.log(toolDescription);
-                        setimgUrl("https://cdn.sanity.io/images/u0v1th4q/production/" + data.ImageUrl);
-                        setwebsiteUrl(data.websiteUrl);
-                    });
-            }
-        } catch (err) {
-            console.log(err);
-        }
-    };
-    useEffect(() => { fetchPrimaryPokemonData(); }, []);
-
-    return (toolDescription != "" ? (
-        <div className={classes.detailscontainer}>
-            <a href={websiteUrl}><div className="toolHeading">{toolname}</div></a>
-            <div className={classes.tooldetailscontainer}>
-                <div className="imagebox"><img src={imgUrl}/></div>
-                <div>{toolDescription}</div>
-                <div>
-                    {websiteUrl}
-                </div>
-            </div>
-        </div>
-    ) :
-        (
-            <CircularProgress
-                color={"success"}
-                size={200}
-            />
-        )
-    );
-}
+return (
+	<div className="SubmitNewTool"> 
+	<Grid>
+	  <Card style={{ maxWidth: 450, padding: "20px 5px", margin: "0 auto" }}>
+		<CardContent>
+		  <Typography gutterBottom variant="h5">
+		  Submit New Tool
+		</Typography> 
+		<Typography variant="body2" color="textSecondary" component="p" gutterBottom>
+			We will review your submitted tool and update the listing.
+		</Typography> 
+		  <form>
+			<Grid container spacing={1}>
+			  <Grid xs={12} sm={6} item>
+				<TextField placeholder="Tool Name" label="Tool Name" variant="outlined" fullWidth required />
+			  </Grid>
+			  <Grid xs={12} sm={6} item>
+				<TextField placeholder="Tool Website" label="Tool Website" variant="outlined" fullWidth required />
+			  </Grid>
+			  <Grid item xs={12}>
+				<TextField label="Tool Short Description" multiline rows={4} placeholder="Enter Tool Short Description" variant="outlined" fullWidth required />
+			  </Grid>
+			  <Grid item xs={12}>
+				<TextField label="Tool Description" multiline rows={4} placeholder="Type Tool's Full Description" variant="outlined" fullWidth required />
+			  </Grid>
+			  <Grid item xs={12}>
+				<TextField type="email" placeholder="Enter email" label="Email" variant="outlined" fullWidth />
+			  </Grid>
+			  <Grid item xs={12}>
+				<Button type="submit" variant="contained" color="primary" fullWidth>Submit</Button>
+			  </Grid>
+			</Grid>
+		  </form>
+		</CardContent>
+	  </Card>
+	</Grid>
+  </div>
+);
+};
 
 export default ToolDetails;
